9056fd1164e88911eab8b9c5c78ef94da71a4eb8
Toddy Mladenov <toddysm@gmail.com>
2023-05-30 20:20:31 -0700

    Added module comment

--- src/regtool/constants.py
+++ src/regtool/constants.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+:mod: `constants` - Module for common constants used throughout the tools
+=========================================================================
+
+    module:: constants
+    :platform: Unix, Windows
+    :synopsis: Module for common constants used throughout the tools
+    moduleauthor:: ToddySM <toddysm@gmail.com>
+"""
+
 import sys
 
 IS_WINDOWS_PLATFORM = (sys.platform == 'win32')
