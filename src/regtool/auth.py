e18fc5992de74981d8a0fc8c7d91ec123ebce9fe
Toddy Mladenov <toddysm@gmail.com>
2023-09-15 19:40:03 -0700

    Preserving work

--- src/regtool/auth.py
+++ src/regtool/auth.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 """
 :mod: `auth` - Module for OCI registry authentication 
@@ -11,29 +10,51 @@
     moduleauthor:: ToddySM <toddysm@gmail.com>
 """
 
-"""
-Authenticate with an OCI registry. Similar to the ``docker login`` command.
+import logging
 
-:param username: The registry user name 
-    (default is None)
-:type username: str
-:param password: The registry password
-    (default is None)
-:type password: str
-:param email: The email for the registry account
-    (default is None)
-:type email: str
-:param registry: The hostname of the registry
-    (default is None)
-:type registry: str
-:param reauth: Whether or not to refresh existing authentication with the 
-    OCI registry
-    (default is False)
-:type reauth: bool
-:param dockercfg_path: Path to Docker config file
-    (default ``$HOME/.docker/config.json`` if present,
-    otherwise ``$HOME/.dockercfg``)
-:returns: The response from the login request
-:rtype: dict
-:raises ExceptionType: 
-"""
+from regtool.errors import *
+
+log = logging.getLogger(__name__)
+
+def login(username, password=None, email=None, registry=None,
+              reauth=False, dockercfg_path=None):
+    """
+    Authenticate with an OCI registry. Similar to the ``docker login`` command.
+    If Docker credential store is used, credentials information is stored in the
+    Docker credential store. If not, the credentials are stores in the following
+    environment variables: ``REGTOOL_REGISTRY``, ``REGTOOL_USERNAME``, and 
+    ``REGTOOL_PASSWORD``
+
+    :param username: The registry user name 
+        (default is None)
+    :type username: str
+    :param password: The registry password
+        (default is None)
+    :type password: str
+    :param email: The email for the registry account
+        (default is None)
+    :type email: str
+    :param registry: The hostname of the registry
+        (default is None)
+    :type registry: str
+    :param reauth: Whether or not to refresh existing authentication with the 
+        OCI registry
+        (default is False)
+    :type reauth: bool
+    :param dockercfg_path: Path to Docker config file
+        (default ``$HOME/.docker/config.json`` if present)
+    :returns: The response from the login request
+    :rtype: dict
+    :raises ExceptionType: 
+    """
+
+def logout(registry=None, dockercfg_path=None):
+    """
+    Logsout the user and removes the stored credentials. If Docker credential 
+    store is used, the credentials are removed from the credential store. If not,
+    the environment variables ``REGTOOL_REGISTRY``, ``REGTOOL_USERNAME``, and 
+    ``REGTOOL_PASSWORD`` are cleared.
+
+    NOTE: It is recommended to logout after every session if Docker credential store
+    is not used.
+    """
--- src/regtool/cli.py
+++ src/regtool/cli.py
@@ -12,8 +12,10 @@
 """
 
 import click
-
 import logging
+import requests
+
+from regtool.utils import dockercredstore
 
 log = logging.getLogger(__name__)
 
@@ -22,19 +24,29 @@ def cli():
     pass
 
 @click.command()
-@click.option('-u','--username', required=True,
+@click.option('-u','--username', required=False,
               prompt='Username', help="Username for the registry.")
-@click.option('-p', '--password', required=True,
+@click.option('-p', '--password', required=False,
               prompt='Password', help="Password for the registry.",
               hide_input=True, confirmation_prompt=True)
-@click.option('-r', '--registry', required=True,
+@click.option('-r', '--registry', required=False,
               prompt='Registry', help="The OCI registry to login to.")
-def login(username, password=None, registry=None):
+@click.option('-l', '--list', required=False, is_flag=True,
+              help="List OCI logins stored in the credstore.")
+def login(username, password=None, registry=None, list=False):
     """
     Authenticate with the OCI registry. This command requires Docker credential
     helper configured on the machine.
     """
+
     print(f"Username: {username} -> Password: {password} -> Registry: {registry}")
+    if list == True:
+        creds = dockercredstore.list()
+        print(creds)
+    else:
+        creds = dockercredstore.get(registry=registry)
+    print(creds)
+
 
 cli.add_command(login)
 
