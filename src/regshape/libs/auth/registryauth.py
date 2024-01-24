4047baa0801937f2bbaeafea2d8e43608993bc73
Toddy Mladenov <toddysm@gmail.com>
2024-01-23 22:08:22 -0800

    Added method for returning basic auth string
    Signed-off-by: Toddy Mladenov <toddysm@gmail.com>

--- src/regshape/libs/auth/registryauth.py
+++ src/regshape/libs/auth/registryauth.py
@@ -10,31 +10,47 @@
     moduleauthor:: ToddySM <toddysm@gmail.com>
 """
 
+import base64
 import requests
+import json
 
 from typing import Optional
 from urllib.parse import parse_qs, urlparse
 
-def signin(
+def get_basic_auth(username: str, password: str):
+    """
+    Returns the basic authentication credentials as Base64 string.
+    :param username: The username to use for authentication
+    :type username: str
+    :param password: The password to use for authentication
+    :type password: str
+    :return: The basic authentication credentials as Base64 string
+    """
+    auth_string = f"{username}:{password}"
+    return base64.b64encode(auth_string.encode("utf-8")).decode("utf-8")
+
+def get_auth_token(
         url: str, 
         username: Optional[str] = None, 
         password: Optional[str] = None
         ) -> str:
     """
-    Sign into an OCI registry.
+    Signs into an OCI registry using the token authentication method. See
+    https://distribution.github.io/distribution/spec/auth/token/ for more.
 
-    :param url: The hostname of the registry
+    :param url: The URL to the registry resource
     :type url: str
     :param username: The username to use for authentication
     :type username: str
     :param password: The password to use for authentication
     :type password: str
+    :return: The authentication token
     """
     print (url, username, password)
     # Make a request to the /v2 endpoint to get the challenge
     response = requests.get(url)
     return_code = response.status_code
-    if return_code != 401:
+    if return_code == 401:
         auth_header = response.headers["www-authenticate"]
     # Parse the www-authenticate header to get the `realm`, `service`, and `scope` parameters
     if auth_header is None:
@@ -46,10 +62,22 @@ def signin(
         parts = [part.split('=') for part in parts]
         parts = dict(parts)
     
+    # TODO: `scope` may be missing from the parts, validate this
+
     params = {
         'service': parts['service'],
         'scope': parts['scope']
     }
 
     # Make a request to the `realm` with `service` and `scope` endpoint to get the token
-    response = requests.get(parts['realm'], params=params, auth=('username', 'password'))
+    response = requests.get(parts['realm'], params=params, auth=(f'{username}', f'{password}'))
+    # parse the response
+    body = json.loads(response.text)
+    # get the auth token
+    token = body['token']
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+    response = requests.get(url, headers=headers)
+    response.text
